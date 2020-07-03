344dae5bfddc4d5912c8b113648f1f245485784f
Roan Bascoe <roanbascoe5@gmail.com>
2020-07-03 01:51:53 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,7 +3,14 @@
 
 def key_for_min_value(name_hash)
   name_hash.each do |key, value|
-  
-  end 
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |key, value|
+    if lowest_value == nil || value < lowest_value
+      lowest_value = value
+      lowest_key = key
+    end
+  end
+  lowest_key
 
 end
